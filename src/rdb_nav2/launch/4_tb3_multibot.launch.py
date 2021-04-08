2329b3eab7bd87fbd9b0f3ce452fe35cb4223fd0
MortenMDahl <mortenmdahl@outlook.com>
2021-04-08 14:59:45 +0200

    Code and license cleanup

--- src/rdb_nav2/launch/4_tb3_multibot.launch.py
+++ src/rdb_nav2/launch/4_tb3_multibot.launch.py
@@ -1,4 +1,7 @@
-# Copyright (c) 2018 Intel Corporation
+#!/usr/bin/env python3
+
+# Copyright 2021 Morten Melby Dahl.
+# Copyright 2021 Norwegian University of Science and Technology.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,8 +14,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""This is all-in-one launch script intended for use by nav2 developers."""
 
 import os
 
